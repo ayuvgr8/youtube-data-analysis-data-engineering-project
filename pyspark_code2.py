f56650fd9aa7e18b51817085baf3176f8b411863
Ayush Verma <49532650+ayuvgr8@users.noreply.github.com>
2024-05-29 16:10:17 +0530

    Update pyspark_code2.py

--- pyspark_code2.py
+++ pyspark_code2.py
@@ -13,14 +13,15 @@ job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
 # Script generated for node AWS Glue Data Catalog
+
 predicate_pushdown = "region in ('ca','gb','us')"
 
-AWSGlueDataCatalog_node1715002193860 = glueContext.create_dynamic_frame.from_catalog(database="youtube-de-raw", table_name="raw_statistics", transformation_ctx="AWSGlueDataCatalog_node1715002193860", push_down_predicate = predicate_pushdown)
+AWSGlueDataCatalog_node1716977071612 = glueContext.create_dynamic_frame.from_catalog(database="de_youtube_raw", table_name="raw_statistics", transformation_ctx="AWSGlueDataCatalog_node1716977071612", push_down_predicate = predicate_pushdown)
 
 # Script generated for node Change Schema
-ChangeSchema_node1715002343294 = ApplyMapping.apply(frame=AWSGlueDataCatalog_node1715002193860, mappings=[("video_id", "string", "video_id", "string"), ("trending_date", "string", "trending_date", "string"), ("title", "string", "title", "string"), ("channel_title", "string", "channel_title", "string"), ("category_id", "long", "category_id", "bigint"), ("publish_time", "string", "publish_time", "string"), ("tags", "string", "tags", "string"), ("views", "long", "views", "bigint"), ("likes", "long", "likes", "bigint"), ("dislikes", "long", "dislikes", "bigint"), ("comment_count", "long", "comment_count", "bigint"), ("thumbnail_link", "string", "thumbnail_link", "string"), ("comments_disabled", "boolean", "comments_disabled", "boolean"), ("ratings_disabled", "boolean", "ratings_disabled", "boolean"), ("video_error_or_removed", "boolean", "video_error_or_removed", "boolean"), ("description", "string", "description", "string"), ("region", "string", "region", "string")], transformation_ctx="ChangeSchema_node1715002343294")
+ChangeSchema_node1716977132492 = ApplyMapping.apply(frame=AWSGlueDataCatalog_node1716977071612, mappings=[("video_id", "string", "video_id", "string"), ("trending_date", "string", "trending_date", "string"), ("title", "string", "title", "string"), ("channel_title", "string", "channel_title", "string"), ("category_id", "long", "category_id", "bigint"), ("publish_time", "string", "publish_time", "string"), ("tags", "string", "tags", "string"), ("views", "long", "views", "bigint"), ("likes", "long", "likes", "bigint"), ("dislikes", "long", "dislikes", "bigint"), ("comment_count", "long", "comment_count", "bigint"), ("thumbnail_link", "string", "thumbnail_link", "string"), ("comments_disabled", "boolean", "comments_disabled", "boolean"), ("ratings_disabled", "boolean", "ratings_disabled", "boolean"), ("video_error_or_removed", "boolean", "video_error_or_removed", "boolean"), ("description", "string", "description", "string"), ("region", "string", "region", "string")], transformation_ctx="ChangeSchema_node1716977132492")
 
 # Script generated for node Amazon S3
-AmazonS3_node1715002287279 = glueContext.write_dynamic_frame.from_options(frame=ChangeSchema_node1715002343294, connection_type="s3", format="glueparquet", connection_options={"path": "s3://youtube-de-cleaned-aws-project-dev/youtube/cleaned_statistics/", "partitionKeys": ["region"]}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_node1715002287279")
+AmazonS3_node1716977147602 = glueContext.write_dynamic_frame.from_options(frame=ChangeSchema_node1716977132492, connection_type="s3", format="glueparquet", connection_options={"path": "s3://de-on-youtube-analysis-cleansed-apsouth1-dev/youtube/raw_statistics/", "partitionKeys": ["region"]}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_node1716977147602")
 
 job.commit()
